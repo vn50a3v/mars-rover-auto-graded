e22c606843c41685b4b1d7f9603e4d6ee77ce1d2
Carly Langlois <cj.langlois.cl@gmail.com>
2020-09-03 14:21:50 -0500

    add karma browserify

--- my.conf.js
+++ my.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
@@ -27,7 +27,10 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    },
+		'**/*[sS]pec.js': [ 'browserify' ]
+	 },
+	 
+	 plugins: ['karma-browserify'],
 
 
     // test results reporter to use
