3035e08f0c3032f030ae4dec88a1e45c31dcce8f
Carly Langlois <cj.langlois.cl@gmail.com>
2020-09-11 10:29:59 -0500

    back to all tests passing

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -9,7 +9,7 @@ describe("Command class", function() {
         new Command();
       },
       {
-        message: 'Command type requireddd.'
+        message: 'Command type required.'
       }
     );
   });
